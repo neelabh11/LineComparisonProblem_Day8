9f28a21d36d5bfbc9e571b553bba2f92f2422ff7
neelabh11 <neelabh.pandey11@gmail.com>
2022-10-12 22:40:06 +0530

    Compare two lines

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace LineComparisonProblem
     {
         public static void Main(string[] args)
         {
-            LineEquality line = new LineEquality();
-            line.CheckTwoLines();
+            CompareLines comp = new CompareLines();
+            comp.CompareTwoLines();
         }
     }
 }
